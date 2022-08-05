eaf48f49b19201489aae5cd27cd3be007ceb4cc4
Gerolmed <werwolf420@yahoo.de>
2022-08-05 18:03:53 +0200

    Added appwrite connection

---
+++ src/AppWriteBridge.js
@@ -0,0 +1,27 @@
+import { Client } from "appwrite";
+import React, {useContext} from "react";
+
+const client = new Client();
+
+// Init your Web SDK
+client
+    .setEndpoint("https://appwrite.software-engineering.education/v1") // Your API Endpoint
+    .setProject("62ed077218d8330d121c") // Your project ID
+;
+
+const bridge = new AppWriteBridge();
+const AppwriteContext = React.createContext(bridge);
+
+class AppWriteBridge {
+
+}
+
+export function WithAppWrite({children}) {
+    return <AppwriteContext.Provider value={bridge}>
+        {children}
+    </AppwriteContext.Provider>;
+}
+
+export function useAppWrite() {
+    return useContext(AppwriteContext);
+}
--- src/index.js
+++ src/index.js
@@ -2,4 +2,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App.js";
-ReactDOM.render(<App />, document.getElementById("root"));
+import {WithAppWrite} from "./AppWriteBridge";
+
+ReactDOM.render(<React.StrictMode>
+    <WithAppWrite>
+        <App />
+    </WithAppWrite>
+</React.StrictMode>, document.getElementById("root"));
