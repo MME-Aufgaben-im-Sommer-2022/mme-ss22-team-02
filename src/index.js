3b06e45ec0a6e60ed04a527f1060c8dfb2612deb
Gerolmed <werwolf420@yahoo.de>
2022-08-15 19:07:05 +0200

    Upgraded to create root

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 
 import React from "react";
-import ReactDOM from "react-dom";
 import App from "./App.js";
 import {WithAppWrite} from "./AppWriteBridge";
 
-ReactDOM.render(<React.StrictMode>
+import { createRoot } from "react-dom/client";
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<React.StrictMode>
     <WithAppWrite>
         <App />
     </WithAppWrite>
-</React.StrictMode>, document.getElementById("root"));
+</React.StrictMode>);
