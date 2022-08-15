b53ad760b98fc0756c8ab531e9c325eba7b2293b
Cybun-s <62816187+Cybun-s@users.noreply.github.com>
2022-08-16 00:03:20 +0200

    Appwrite Create Account Session

--- src/AppwriteClient.js
+++ src/AppwriteClient.js
@@ -2,15 +2,22 @@ import {Account, Client} from "appwrite";
 import {Observable} from "./utils/Observable";
 
 const client = new Client();
+const account = new Account(client);
 
 // Init your Web SDK
 client
-    .setEndpoint("https://appwrite.software-engineering.education/v1") // Your API Endpoint
+    .setEndpoint("https://appwrite.software-engineering.education/v1/account") // Your API Endpoint
     .setProject("62ed077218d8330d121c") // Your project ID
+    
 ;
 
-const account = new Account(client);
+const promise = account.create(1, 'alia@bit.de', 'password');
 
+promise.then(function (response) {
+    console.log(response); // Success
+}, function (error) {
+    console.log(error); // Failure
+});
 
 export class AppWriteBridge extends Observable {
 
