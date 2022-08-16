8c1bfb0864aca391d41489c0bff7b46dfe76fa35
Jonas <j.freyberg@gmx.de>
2022-08-16 18:29:21 +0200

    Create Community.js
    Community Gerüst

---
+++ src/Community.js
@@ -0,0 +1,70 @@
+export class Community {
+    _id;
+
+    constructor(id) {
+        this._id= id;
+    }
+
+    getCommunityID() {
+
+    }
+    getMemberList() {
+
+    }
+    getMemberCounter() {
+
+    }
+    viewMemberDetails() {
+
+    }
+    openChat() {
+
+    }
+    
+
+
+
+
+    //requests
+    displayOpenRequests() {
+
+    }
+    addNewRequest() {
+
+    }
+    acceptRequest() {
+
+    }
+    viewMyRequests() {
+
+    }
+    viewAcceptedRequests() {
+
+    }
+
+
+
+
+    //suchoptionen
+
+    showAllFilters() {
+
+    }
+    addFilter() {
+
+    }
+    searchByAttribute(...attribute) {
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
