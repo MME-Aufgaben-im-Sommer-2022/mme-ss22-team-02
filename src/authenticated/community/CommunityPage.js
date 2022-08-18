13cfc43798b7d856d0bf0a1eee916a85cac93a1e
Jonas <j.freyberg@gmx.de>
2022-08-18 19:32:54 +0200

    Added Tabs to Main page OpenRequests and MyRequests

--- src/authenticated/community/CommunityPage.js
+++ src/authenticated/community/CommunityPage.js
@@ -1,13 +1,52 @@
 import React from "react";
+import "./CommunityPage.css";
+import {Tab, Tabs} from "@mui/material";
+import MyRequests from "./MyRequests";
+import OpenRequests from "./OpenRequests";
 
+function a11yProps(index) {
+    return {
+        id: `simple-tab-${index}`,
+        'aria-controls': `simple-tab panel-${index}`,
+    };
+}
+function TabPanel({value, index, children}){
+    return <div hidden={value!== index}>
+        {value===index && children}
+    </div>;
+}
 /**
  * @param community {require(../../model/Community)}
  * @return {JSX.Element}
  * @constructor
  */
 export default function CommunityPage({community}) {
+    const [value, setValue] = React.useState(0);
 
-    return <div>
-        community {community.getId()}
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
+
+    return <div className={"main-page"}>
+        <div className={"community-body"}>
+            <div className={"community-tabs"}>
+                <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+                    <Tab label="Open Requests" {...a11yProps(0)} />
+                    <Tab label="My Requests" {...a11yProps(1)} />
+                </Tabs>
+            </div>
+            <TabPanel value={value} index={0}>
+                <OpenRequests/>
+            </TabPanel>
+            <TabPanel value={value} index={1}>
+                <MyRequests/>
+            </TabPanel>
+
+        </div>
+        <div>
+            {/* add MemberList here if needed */}
+        </div>
+
     </div>;
 }
---
+++ src/authenticated/community/OpenRequests.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function OpenRequests (){
+    return <div>
+    Open Requests
+    </div>;
+
+}
