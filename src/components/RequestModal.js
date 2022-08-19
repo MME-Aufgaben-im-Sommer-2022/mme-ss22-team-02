69e2b44574fee98d4604bf7fbf06ae7513f72e66
Jenny295 <76103544+Jenny295@users.noreply.github.com>
2022-08-19 17:56:38 +0200

    Added RequestModal

---
+++ src/components/RequestModal.js
@@ -0,0 +1,46 @@
+import React from "react";
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import { Input } from "@mui/material";
+
+
+const RequestModal = ({open, onClose, title, subTitle}) => {
+    const style = {
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: 400,
+        bgcolor: 'background.paper',
+        border: '2px solid #000',
+        boxShadow: 24,
+        p: 4,
+      };
+    return (
+        <Modal open={open} onClose={onClose} >
+            <Box sx={style}>
+                <Typography id="modal-modal-title" variant="h6" component="h2">
+                    New Request
+                    {title}
+                </Typography>
+                <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+
+                    {subTitle}
+                </Typography>
+                <Box>
+                    <Input placeholder="Article"/>
+                    <Input placeholder="Article2"/>
+                    <Input placeholder="Article3"/>
+                </Box>
+                <Box>
+                    <Button variant={"contained"} onClick={onClose}>Cancel</Button>
+                    <Button variant={"contained"} onClick={onClose}>Next</Button>
+                </Box>
+            </Box>
+        </Modal>
+    );
+};
+
+export default RequestModal;
