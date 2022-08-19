599cb30b04b74d302b9f5b4a85c07b1c0d06bfa9
Jonas <j.freyberg@gmx.de>
2022-08-19 20:02:44 +0200

    Added RequestCard UI

--- src/authenticated/community/OpenRequests.js
+++ src/authenticated/community/OpenRequests.js
@@ -1,8 +1,28 @@
 import React from "react";
+import RequestCard from "./RequestCard";
 
 export default function OpenRequests (){
+    const requests = [
+        {
+            id: "1",
+            test: "abc",
+        },
+        {
+            id: "2",
+        },
+        {
+            id: "3",
+        },
+        {
+            id: "4",
+        },
+    ];
+
     return <div>
-    Open Requests
+        {
+            requests.map((value) => <RequestCard key={value.id} {...value}/>)
+        }
+
     </div>;
 
 }
---
+++ src/authenticated/community/RequestCard.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+
+export default function RequestCard({id,test}){
+    return <div>
+        test {id}{test}
+    </div>;
+
+}
