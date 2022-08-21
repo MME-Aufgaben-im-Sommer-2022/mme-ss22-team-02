3ec6f6125a7c2824eadccdc7db795c093878db1b
Jonas <j.freyberg@gmx.de>
2022-08-21 11:23:36 +0200

    Cards displayed in Grid

--- src/authenticated/community/OpenRequests.js
+++ src/authenticated/community/OpenRequests.js
@@ -1,28 +1,35 @@
 import React from "react";
 import RequestCard from "./RequestCard";
+import "./CommunityPage.css";
+import {Container, Grid} from "@mui/material";
 
 export default function OpenRequests (){
     const requests = [
         {
             id: "1",
             test: "abc",
+            date: "02.09.2022",
         },
         {
             id: "2",
+            date: "05.09.2022",
         },
         {
             id: "3",
+            date: "09.09.2022",
         },
         {
             id: "4",
+            date: "31.08.2022",
         },
     ];
 
-    return <div>
+    return<Container>
+        <Grid container columns={{ xs: 4, sm: 8, md: 12 }}>
         {
-            requests.map((value) => <RequestCard key={value.id} {...value}/>)
+            requests.map((value) => <Grid key={value.id} xs={4} md={3}><RequestCard className={"grid-card"} {...value}/></Grid>)
         }
-
-    </div>;
+        </Grid>
+    </Container>;
 
 }
--- src/authenticated/community/RequestCard.js
+++ src/authenticated/community/RequestCard.js
@@ -1,9 +1,40 @@
 import React from "react";
+import {Avatar, Button, Card, CardActions, CardContent, CardHeader, IconButton, Typography} from "@mui/material";
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 
 
-export default function RequestCard({id,test}){
-    return <div>
-        test {id}{test}
-    </div>;
+
+
+export default function RequestCard({id,test,date,className}){
+
+        return (
+        <Card className={className}>
+            <CardHeader
+                avatar={
+                    <Avatar aria-label="recipe">
+                        R
+                    </Avatar>
+                }
+                action={
+                    <IconButton aria-label="settings">
+                        <MoreVertIcon />
+                    </IconButton>
+                }
+                title= {id}
+                subheader={date}
+            />
+            <CardContent>
+                <Typography variant="body2" color="text.secondary">
+                    {test}
+                </Typography>
+            </CardContent>
+            <CardActions disableSpacing>
+
+                <Button variant="outlined">Bring ich mit</Button>
+            </CardActions>
+        </Card>
+        );
+
+
 
 }
