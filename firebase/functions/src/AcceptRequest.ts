967176705c2d1df736685fa56ab1736c4fd98618
Cybun-s <62816187+Cybun-s@users.noreply.github.com>
2022-09-09 17:44:08 +0200

    Accept request

---
+++ firebase/functions/src/AcceptRequest.ts
@@ -0,0 +1,48 @@
+// welche Daten
+// was verändern
+// Tipp: User (aus context) angemeldet, requestId, communityId
+// CreateCommunity
+
+import * as functions from "firebase-functions";
+import FirebaseAdmin from "./FirebaseAdmin";
+
+
+// onCall request zum Server (alles ausser Imports)
+// arrays sind objects
+export default functions.https.onCall(async (data, context) => {
+    if (!context.auth) return;
+    if (typeof data !== "object") return;
+
+    const requestId = data.requestId;
+    const communityId = data.communityId;
+
+    if (typeof requestId !== "string") return;
+    if (typeof communityId !== "string") return;
+
+
+    const firestore = FirebaseAdmin.firestore();
+
+    const requestDoc = firestore
+        .collection(`communities/${communityId}/requests`)
+        .doc(requestId);
+
+    const requestSnapshot = await requestDoc.get();
+    const requestData = requestSnapshot.data();
+    if (!requestData) return;
+
+    if (requestData.state !== "OPEN" ) {
+        return {
+            error: "Request is not open",
+        };
+    }
+
+    await requestDoc.update({
+        state: "IN_PROGRESS",
+        bringer: context.auth.uid,
+    });
+    return {
+        success: true,
+    };
+});
+
+
